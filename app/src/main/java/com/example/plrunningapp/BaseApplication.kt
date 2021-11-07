8da325dd5fd863d53d56cca467c77547145b9dcd
Brian Donnoe <dunow33@hotmail.com>
2021-11-07 02:58:48 -0500

    getting the database working with hilt

---
+++ app/src/main/java/com/example/plrunningapp/BaseApplication.kt
@@ -0,0 +1,14 @@
+package com.example.plrunningapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
--- app/src/main/java/com/example/plrunningapp/MainActivity.kt
+++ app/src/main/java/com/example/plrunningapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.plrunningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.plrunningapp.db.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}")
     }
 }
