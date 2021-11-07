75b4bfe0c8f287109b03aacfb6ea6320371a7512
Brian Donnoe <dunow33@hotmail.com>
2021-11-07 18:46:55 -0500

    stuck on video 8

--- app/src/main/java/com/example/plrunningapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/plrunningapp/ui/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.example.plrunningapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.findNavController
 import com.example.plrunningapp.R
+import com.example.plrunningapp.databinding.ActivityMainBinding
 import com.example.plrunningapp.db.RunDao
+import com.example.plrunningapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDao
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomNavigationView
+        navController = findNavController(R.id.nav_graph)
+
+        setSupportActionBar(toolbar)
+
+
+        navView.setupWithNavController(navController)
+
+        navHostFragment
     }
 }
